cdc236957d5098a6d6cb5ab22529e3e6f0f75e54
Fozail Ahmad <fozail.ahmad@mail.mcgill.ca>
2024-02-28 02:42:21 -0500

    Removed Debugs

--- query_generator.py
+++ query_generator.py
@@ -31,7 +31,7 @@ class RandomCypherGenerator():
     _other = ""
 
     def __init__(self, node_labels, edge_labels, node_properties, connectivity_matrix):
-        print("Initializing Query Generator with Schema")
+        # print("Initializing Query Generator with Schema")
         config = configparser.ConfigParser()
         config.read('graphgenie.ini')
         self.graphdb = config['default']['graphdb']
@@ -59,7 +59,7 @@ class RandomCypherGenerator():
 
     # call before each run of test
     def init(self):
-        print("Initializing Query Generator without Schema")
+        # print("Initializing Query Generator without Schema")
         config = configparser.ConfigParser()
         config.read('graphgenie.ini')
         self._node_num = int(config['testing_configs']['_node_num'])
@@ -70,7 +70,7 @@ class RandomCypherGenerator():
 
     # note: call before each generation
     def init_query(self):
-        print("Initializing Query Function")
+        # print("Initializing Query Function")
         self._match = ""
         self._path = ""
         self._predicate = ""
@@ -85,7 +85,7 @@ class RandomCypherGenerator():
     # this is a random choice api for a given rate
     # e.g., if given_rate = 0.3, then 30% returns true and 70% return false
     def random_choice(self, given_rate):
-        print("Random Choice Function")
+        # print("Random Choice Function")
         if randint(1,100)<=given_rate*100:
             return True
         else:
@@ -94,18 +94,18 @@ class RandomCypherGenerator():
     # this is a random symbol generator
     # note: we only consider lowercase letters in ascii
     def random_symbol(self):
-        print("Random Symbol Function")
+        # print("Random Symbol Function")
         return ''.join(choice(string.ascii_lowercase) for _ in range(self.random_symbol_len))
 
     # _match indicates the query is a graph-matching query rather than add/update/delete queries
     # cypher: `match` or `optional match` clause
     def match_generator(self):
-        print("Match Generator Function")
+        # print("Match Generator Function")
         match_candidates = ["MATCH", "OPTIONAL MATCH"]
         self._match = choice(match_candidates)
 
     def random_node_multi_labels(self):
-        print("Random Node Multi Labels Function")
+        # print("Random Node Multi Labels Function")
         label_num = len(self.node_labels)
         random_num = randint(2, label_num)
         node_label = choice(self.node_labels)
@@ -114,7 +114,7 @@ class RandomCypherGenerator():
         return node_label
 
     def random_edge_types(self):
-        print("Random Edge Types Function")
+        # print("Random Edge Types Function")
         type_num = len(self.edge_labels)
         random_num = randint(2, type_num)
         edge_type = choice(self.edge_labels)
@@ -124,7 +124,7 @@ class RandomCypherGenerator():
 
     # given the previous node, use connectivity matrix to find connectable node labels
     def connectable_node_labels(self, prev_node_label, prev_node_direction):
-        print("Connectable Node Labels Function")
+        # print("Connectable Node Labels Function")
         if self.graphdb!="neo4j":
             return self.node_labels
         if prev_node_label==None or prev_node_label=="" or "%" in self.node_labels:
@@ -147,7 +147,7 @@ class RandomCypherGenerator():
 
     # to generate random path unit
     def random_path_unit(self, prev_node_label=None, prev_node_direction=None):
-        print("Random Path Unit Function")
+        # print("Random Path Unit Function")
         connectable_node_labels = self.connectable_node_labels(prev_node_label, prev_node_direction)
         path_unit_candidates = [
             "({node_sym})-[{edge_sym}]-",
@@ -172,7 +172,7 @@ class RandomCypherGenerator():
         return random_unit.format(node_sym=random_node_sym, edge_sym=random_edge_sym)
 
     def cypher_get_unit_direction(self, path_unit):
-        print("Cypher Get Unit Direction Function")
+        # print("Cypher Get Unit Direction Function")
         if "<" in path_unit:
             return "<"
         elif ">" in path_unit:
@@ -181,7 +181,7 @@ class RandomCypherGenerator():
             return "-"
 
     def parse_path_unit_node_label(self, path_unit):
-        print("Parser Path Unit Node Label Function")
+        # print("Parser Path Unit Node Label Function")
         the_node = path_unit.split('-')[0]
         if ':' not in the_node:
             return ""
@@ -191,7 +191,7 @@ class RandomCypherGenerator():
 
     # it is important to generate diverse graph query patterns
     def path_generator(self):
-        print("Path Generator Function")
+        # print("Path Generator Function")
         nodes_num = self._node_num
         path = ""
         prev_node_label = ""
@@ -221,17 +221,17 @@ class RandomCypherGenerator():
             )
             path = ("({cyc})-{path}-({cyc})".format(cyc=cyclic_str, path="-".join(path.split("-")[1:-1])))
         self._path = path
-        print("Calling Path Parser Function")
+        # print("Calling Path Parser Function")
         self.path_parser()
-        print("Path Parser Function Returned")
+        # print("Path Parser Function Returned")
 
     def path_parser(self):
-        print("Path Parser Function")
+        # print("Path Parser Function")
         self.nodes_num = self._path.count('-')/2 + 1
         symbol_list = re.findall("[(,\[][a-z]{{{sym_len}}}".format(sym_len=self.random_symbol_len), self._path)
         symbol_list_with_labels = re.findall("[(,\[][a-z]{{{sym_len}}}[:]?[\w]+".format(sym_len=self.random_symbol_len), self._path)
         for each_symbol in symbol_list:
-            print("looping through symbols")
+            # print("looping through symbols")
             symbol_name = each_symbol[1:]
             self.symbols.append(symbol_name)
             if each_symbol[0]=="(":
@@ -239,7 +239,7 @@ class RandomCypherGenerator():
             elif each_symbol[0]=="[":
                 self.edge_symbols.append(symbol_name)
         for each_symbol in symbol_list_with_labels:
-            print("looping through symbols with labels")
+            # print("looping through symbols with labels")
             symbol_name = each_symbol[1:].split(':')[0]
             symbol_label = each_symbol[1:].split(':')[1]
             self.symbols.append(symbol_name)
@@ -252,61 +252,61 @@ class RandomCypherGenerator():
         # record node labels only, save as vector.
         all_nodes = re.findall("\([A-Za-z0-9:]*\)", self._path)
         path_vector = []
-        print("looping through nodes - start")
+        # print("looping through nodes - start")
         counter = 0
         for each_node in all_nodes:
-            print("Iteration #" + str(counter))
+            # print("Iteration #" + str(counter))
             counter += 1
             if ':' not in each_node:
                 path_vector.append(0)
             else:
-                print("Slow Part Starts V1")
+                # print("Slow Part Starts V1")
                 node_label = each_node.split(':')[1].split(')')[0]
                 path_vector.append(self.node_labels.index(node_label)+1)
-                print("Slow Part Ends V1")
-        print("looping through nodes - end - count = " + str(counter))
+                # print("Slow Part Ends V1")
+        # print("looping through nodes - end - count = " + str(counter))
 
         # the code below is for incremental base query generation
         # we encode the graph pattern into vectors
         # too many deduplicated queries would increase the node number of graph pattern
 
         if path_vector not in self._path_vectors:
-            print("Slow Part Starts V2")
-            print("node num: {} tested vectors:{}".format(self._node_num, self._last_vector_length+1))
+            # print("Slow Part Starts V2")
+            # print("node num: {} tested vectors:{}".format(self._node_num, self._last_vector_length+1))
             self._path_vectors.append(path_vector)
-            print("Slow Part Ends V2")
+            # print("Slow Part Ends V2")
         
         if self._last_vector_length == len(self._path_vectors):
-            print("Slow Part Starts V3")
+            # print("Slow Part Starts V3")
             self.stuck += 1
-            print("Slow Part Ends V3")
+            # print("Slow Part Ends V3")
         else:
-            print("Slow Part Starts V4")
+            # print("Slow Part Starts V4")
             self.stuck = 0
-            print("Slow Part Ends V4")
+            # print("Slow Part Ends V4")
 
-        print("Slow Part Starts V5")
+        # print("Slow Part Starts V5")
         self._last_vector_length = len(self._path_vectors)
-        print("Slow Part Ends V5")
+        # print("Slow Part Ends V5")
         
         if self.stuck==2*self._node_num*self._node_num:
-            print("Slow Part Starts V6")
+            # print("Slow Part Starts V6")
             self.stuck = 0
             self._node_num += 1
             self._last_vector_length = 0
             self._path_vectors.clear()
-            print("Slow Part Ends V6")
+            # print("Slow Part Ends V6")
 
     # TODO: add more predicate
     def predicate_generator(self):
-        print("Predicate Generator Function")
+        # print("Predicate Generator Function")
         pattern = "WHERE {}"
         predicate = "{} IS NOT NULL AND True".format(choice(self.node_symbols)) if len(self.node_symbols)>0 else "True"
         self._predicate = pattern.format(predicate)
 
     # note: we focus on testing `count`
     def return_generator(self):
-        print("Return Generator Function")
+        # print("Return Generator Function")
         _return = "{return_keyword} {return_staff}"
         return_keywords = ["RETURN", "RETURN DISTINCT"]
         # TODO: to support count(DISTINCT ), max(), min()
@@ -319,7 +319,7 @@ class RandomCypherGenerator():
 
     # for count() testing, we do not really need other clauses
     def other_generator(self):
-        print("Other Generator Function")
+        # print("Other Generator Function")
         _other = "{order_by} {skip} {limit}"
         order_by_keywords = ["", "ORDER BY -1+1", "ORDER BY NULL"]
         skip_keywords = ["", "SKIP 1", "SKIP 0", "SKIP 0", "SKIP 0", "SKIP 0", "SKIP 0", "SKIP 0",]
@@ -331,27 +331,27 @@ class RandomCypherGenerator():
         )
 
     def random_query_generator(self):
-        print("***Random Query Generator Function***")
-        print("Calling Initializing Query Function")
+        # print("***Random Query Generator Function***")
+        # print("Calling Initializing Query Function")
         self.init_query()
-        print("Initializing Query Function Returned")
-        print("Calling Match Generator Function")
+        # print("Initializing Query Function Returned")
+        # print("Calling Match Generator Function")
         self.match_generator()
-        print("Match Generator Function Returned")
-        print("Calling Path Generator Function")
+        # print("Match Generator Function Returned")
+        # print("Calling Path Generator Function")
         self.path_generator()
-        print("Path Generator Function Returned")
-        print("Calling Predicate Generator Function")
+        # print("Path Generator Function Returned")
+        # print("Calling Predicate Generator Function")
         self.predicate_generator()
-        print("Predicate Generator Function Returned")
-        print("Calling Return Generator Function")
+        # print("Predicate Generator Function Returned")
+        # print("Calling Return Generator Function")
         self.return_generator()
-        print("Return Generator Function Returned")
-        print("Calling Other Generator Function")
+        # print("Return Generator Function Returned")
+        # print("Calling Other Generator Function")
         self.other_generator()
-        print("Return Other Function Returned")
+        # print("Return Other Function Returned")
 
-        print("Create Query Base")
+        # print("Create Query Base")
         query = self.cypher_query_pattern.format(
             _match = self._match,
             _path = self._path,
@@ -360,8 +360,8 @@ class RandomCypherGenerator():
             _other = self._other
         )
 
-        print("Format Query")
+        # print("Format Query")
         query = re.sub(' +', ' ', query).strip(' ')
         query = re.sub('[*]+', '*', query).strip(' ')
-        print("Returning Query")
+        # print("Returning Query")
         return query
